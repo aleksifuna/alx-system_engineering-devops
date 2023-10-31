60abcccb6299684b811ce0c1db9c5269e6684d50
Alex Sifuna <aleksifuna@gmail.com>
2023-10-31 21:15:08 +0000

    added a ruby script that parses log files

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,5 +1,3 @@
 #!/usr/bin/env ruby
-results = ARGV[0].scan(/from:([a-zA-Z0-9+]+)\].*?to:([0-9+]+)\].*?flags:([-:0-9]+)\]/)
-if results.any?
-  puts results.map { |match| match.join(',') }.join("\n")
-end
+results = ARGV[0].scan(/from:([a-zA-Z0-9+]+)\].*to:([0-9+]+)\].*flags:([-:0-9]+)\]/)
+puts results.map { |match| match.join(',') }.join("\n")
