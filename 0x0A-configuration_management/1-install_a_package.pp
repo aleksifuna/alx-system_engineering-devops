a863b90442f627226d1f9cc7da6d5a64da2d211a
aleksifuna <aleksifuna@gmail.com>
2023-12-22 23:30:15 +0000

    added a manifest that installs flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,16 @@
 # installs flask from pip3
 
+# Install pip
+package { 'python3-pip':
+  ensure => present,
+}
+# Install Flask 2.1.0
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
-  require  => Package['python3-pip']
+}
+# Install werkzeug 2.1.1
+package { 'werkzeug':
+ensure   => '2.1.1',
+provider => 'pip3'
 }
