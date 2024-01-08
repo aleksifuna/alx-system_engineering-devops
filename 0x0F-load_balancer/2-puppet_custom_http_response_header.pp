d0f1ed5bf6505f5334fc3b809e84eceac328873e
Alex Sifuna <aleksifuna@gmail.com>
2024-01-08 22:59:20 +0000

    added a puppet script to configure a server and add a customer header

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,27 @@
+# installs and configures a nginx server
+
+exec { 'update_package_list':
+  command  => 'sudo apt-get update',
+  provider => 'shell',
+}
+
+package { 'nginx':
+  ensure => present,
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+file_line { 'header line':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "	location / {
+  add_header X-Served-By ${hostname};",
+  match  => '^\tlocation / {',
+}
+
+exec { 'restart service':
+  command  => 'sudo service nginx restart',
+  provider => shell,
+}
